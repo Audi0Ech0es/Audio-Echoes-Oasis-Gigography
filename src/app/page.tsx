039b103b2f01856bf3233d902b2590124501a9da
Matt Aruldoss Hines <mahsynnovis@gmail.com>
2025-06-10 09:45:16 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <h1 class="text-4x

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,9 +11,7 @@ export default async function HomePage() {
       <main className="flex-grow container mx-auto px-4 py-8 max-w-7xl">
         <header className="mb-10 md:mb-16 text-center">
           
-          <h1 className="text-4xl sm:text-5xl md:text-6xl font-headline font-bold text-foreground tracking-tight mt-6">
-            Oasis<span className="text-primary">Gigography</span>
-          </h1>
+          
           <p className="mt-4 text-md sm:text-lg text-muted-foreground max-w-3xl mx-auto">
             Definitely Maybe... You Were There. Find Your Gig.
           </p>
@@ -28,3 +26,4 @@ export default async function HomePage() {
     </div>
   );
 }
+
